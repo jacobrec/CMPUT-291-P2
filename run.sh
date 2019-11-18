781e95d9e2ecd47ef33e5002b3de68e244991b5e
Jacob Reckhard <jacobreckhard@gmail.com>
2019-11-18 11:41:03 -0700

    better testing

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
-cd src
-cd c
-make
-cd ../
+#!/bin/bash
 
-cat 10.xml | racket phase1.rkt
+cd src/c
+make
+cd ../ # in src
+
+cd ../testing
+
+echo "Testing Phase 1"
+function comp () {
+    diff $2-$1.txt $1.txt
+}
+function testPhase1 () {
+    cat $1.xml | racket ../src/phase1.rkt
+    comp terms $1
+    comp emails $1
+    comp dates $1
+    comp recs $1
+}
+# testPhase1 10
+testPhase1 1k
+echo "Phase 1 complete"
