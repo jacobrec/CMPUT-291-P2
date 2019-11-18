e55e19b473f1634b883fd36bf66617b5f289ed73
Jacob Reckhard <jacobreckhard@gmail.com>
2019-11-18 12:48:06 -0700

    make more shell scripts

--- runTest.sh
+++ runTest.sh
@@ -10,15 +10,16 @@ function comp () {
     diff $2-$1.txt $1.txt
 }
 function testProject () {
-    # Phase 1
-    cat $1.xml | racket ../src/phase1.rkt
+    cd ../
+    ./buildAndRun.sh $1
+    cd testing
+
+    # Test Phase 1
     comp terms $1
     comp emails $1
     comp dates $1
     comp recs $1
 
-    # Phase 2
-    ../src/phase2.sh
 }
 testProject 10
 # testProject 1k
