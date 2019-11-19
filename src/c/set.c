edde3078c5827bb70882c7238cdb056cda3bc543
Jacob Reckhard <jacobreckhard@gmail.com>
2019-11-19 14:24:36 -0700

    added intersect and union methods

--- src/c/set.c
+++ src/c/set.c
@@ -11,6 +11,7 @@ Set* set_new_size(int bl) {
     s->buckets = malloc(sizeof(int) * s->bucketLength);
     memset(s->buckets, 0, sizeof(int) * s->bucketLength);
     s->size = 0;
+    s->isUsed = true;
 }
 void rebuild(Set* set) {
     Set* ns = set_new_size(set->bucketLength * 3.14);
@@ -67,6 +68,7 @@ void set_add(Set* set, int item) {
 
 void set_remove(Set* set, int item) {
     int* ptr;
+    set->isUsed = true;
     if (find(set, item, &ptr)) {
         *ptr = -1;
     }
@@ -81,15 +83,35 @@ void set_clear(Set* s) {
     s->bucketLength = 100;
     s->size = 0;
     s->buckets = realloc(s->buckets, sizeof(int) * s->bucketLength);
+    s->isUsed = false;
     memset(s->buckets, 0, sizeof(int) * s->bucketLength);
 }
 
 void set_intersect(Set* set, Set* other) {
-
+    Set* ns = set_new();
+    for (int i = 0; i < other->bucketLength; i++) {
+        if (other->buckets[i] > 0) {
+            if (set_has(set, other->buckets[i])) {
+                set_add(ns, other->buckets[i]);
+            }
+        }
+    }
+    set_delete(other);
+    free(set->buckets);
+    set->buckets = ns->buckets;
+    set->size = ns->size;
+    set->bucketLength = ns->bucketLength;
+    set->isUsed = true;
 }
 
 void set_union(Set* set, Set* other) {
-
+    for (int i = 0; i < other->bucketLength; i++) {
+        if (other->buckets[i] > 0) {
+            set_add(set, other->buckets[i]);
+        }
+    }
+    set->isUsed = true;
+    set_delete(other);
 }
 
 void set_delete(Set* set) {
--- src/c/test_set.c
+++ src/c/test_set.c
@@ -30,5 +30,55 @@ int test_set() {
     assert(set_has(s, 1002));
     assert(set_has(s, 10002));
 
+    set_delete(s);
+
+    // Test union
+    Set* a = set_new();
+    Set* b = set_new();
+    set_add(a, 1);
+    set_add(a, 2);
+    set_add(a, 3);
+    set_add(b, 3);
+    set_add(b, 4);
+    set_add(b, 5);
+
+    set_union(a, b);
+    assert(set_has(a, 1));
+    assert(set_has(a, 2));
+    assert(set_has(a, 3));
+    assert(set_has(a, 4));
+    assert(set_has(a, 5));
+    int items = 0;
+    for(int i = 0; i < a->bucketLength; i++) {
+        if (a->buckets[i] > 0) {
+            items++;
+        }
+    }
+    assert(items == 5);
+
+    set_clear(a);
+    b = set_new();
+    set_add(a, 1);
+    set_add(a, 2);
+    set_add(a, 3);
+    set_add(b, 3);
+    set_add(b, 4);
+    set_add(b, 5);
+    set_intersect(a, b);
+    assert(!set_has(a, 1));
+    assert(!set_has(a, 2));
+    assert(set_has(a, 3));
+    assert(!set_has(a, 4));
+    assert(!set_has(a, 5));
+    items = 0;
+    for(int i = 0; i < a->bucketLength; i++) {
+        if (a->buckets[i] > 0) {
+            items++;
+        }
+    }
+    assert(items == 1);
+
+    set_delete(a);
+
     fprintf(stderr, "Tested Set Implementation\n");
 }
