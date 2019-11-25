7712bd4e164c37df4a01ee2c2c680be8354b6501
Radomir <rasdasdasd.goa@gmail.com>
2019-11-25 02:52:39 -0700

    Fixed duplicate row ids when making 1 million entry xml

--- makeMil.sh
+++ makeMil.sh
@@ -4,7 +4,7 @@ cd testing
 cat 100k.xml | head -2 > 1m.xml
 
 for i in {1..10}; do
-  cat 100k.xml | tail -n +3 | head -n -1 >> 1m.xml
+    cat 100k.xml | tail -n +3 | head -n -1 | grep -Po "(?<=</row>).*" | awk -v i="$i" '{printf "<mail><row>%d</row>%s\n", NR+((i-1)*100000), $0}' >> 1m.xml
   echo "${i}0%"
 done
 
